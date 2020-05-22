14bec722deb0bde6a1810bb78b021a11ee18789e
CamilaBarbareschi <camilabarbareschi@outlook.com>
2020-05-22 20:18:50 -0300

    detalles y correción del tamaño de las imagenes

--- index/index.js
+++ index/index.js
@@ -5,17 +5,9 @@ window.addEventListener ("load", function (){
     boton.onclick = function change () {
     boton.addEventListener("click", function () {
       document.querySelector("section.rankings").style.display = "block";
-    });
-    boton.addEventListener("click", function () {
       document.querySelector("div.animacionWrapper").style.display = "none";
-    });
-    boton.addEventListener("click", function () {
       document.querySelector("div.btnback").style.display = "block";
-    });
-    boton.addEventListener("click", function () {
       document.querySelector("div.btnback").style.display = "grid";
-    });
-    boton.addEventListener("click", function () {
       document.querySelector("div.btnback").style.gridTemplateColumns = "auto auto";
     });
     }
@@ -23,19 +15,20 @@ window.addEventListener ("load", function (){
     var goGoback = document.querySelector("div.circulo")
     
     goGoback.addEventListener ("mouseover", function(){
-        document.querySelector("div.circulo").style.backgroundColor = "#dbff4b"
+      goGoback.style.backgroundColor = "#dbff4b"
     })  
     goGoback.addEventListener ("mouseout", function(){
-        document.querySelector("div.circulo").style.backgroundColor = "gray"
+        goGoback.style.backgroundColor = "gray"
     })  
 
     var goGobackTexto = document.querySelector("div.textoBack")
     
     goGobackTexto.addEventListener ("mouseover", function(){
-        document.querySelector("div.textoBack").style.color = "#dbff4b"
+      goGobackTexto.style.color = "#dbff4b"
     })  
     goGobackTexto.addEventListener ("mouseout", function(){
-        document.querySelector("div.textoBack").style.color = "gray"
+      goGobackTexto.style.color = "gray"
     })  
- 
+
+   
 })
