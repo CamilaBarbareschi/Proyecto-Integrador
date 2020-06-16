e93546041bab86156f7e3e0aa89aef8f848c4461
Santiagolemoine <slemoine@udesa.edu.ar>
2020-06-16 14:32:11 -0400

    js

--- playlist/playlist.js
+++ playlist/playlist.js
@@ -5,39 +5,43 @@
 
         let queryString = new URLSearchParams(location.search);
     
-        let codigoDeplaylist = queryString.get("idPlaylist");
+        let codigoDeplaylist = queryString.get("id-playlist");
      
     
-        fetch( "https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/playlists" + codigoDeplaylist )
+        fetch( "https://cors-anywhere.herokuapp.com/https://api.deezer.com/playlist/" + codigoDeplaylist )
         .then(
             function(respuesta) {
                 return respuesta.json();            
             }
         )
         .then(
-            function(resultado) {
-                console.log (resultado)
-                let playlist = resultado.data;
-                
+            function(informacion) {
+                console.log (informacion)
+                let playlists = informacion.tracks.data;
+    
                 for (let index = 0; index < 9; index++) {
-                    const cadaCancion = playlist[index];
-                    
-                let title = cadaCancion.title;;
-                let artist = cadaCancion.name;
-                let album = cadaCancion.name;
+    
+                const cadaplaylist = playlists[index];
+                
+                
+                let song = cadaplaylist.title;;
+                let artist = cadaplaylist.artist.name;
+                let album = cadaplaylist.album.title;
     
                     let htmlNuevo = `
-                    <article class="musica">
-                    <div><button><i class="fas fa-heart"></i></button></div>
+                    <div class="musica">
                     <div><i class="fas fa-play"></i></div>
-                    <div> ` + title +` </div>
+                    <div> ` + song +` </div>
                     <div> <a href="../artistas/artistas.html"> ` + artist +` </a> </div>
                     <div> <a href="../albums/albums.html"> ` + album +` </a></div>
                     </article>
+                    </div>
                     `
                     
-                    document.querySelector("playlist").innerHTML += htmlNuevo
-                }
-            }
+                    document.querySelector(".playlistss").innerHTML += htmlNuevo
+                 }
+             }
         )
     })
+
+    
