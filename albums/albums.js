1d3dd37519b5c3475eaaf60d21650d983a8ad168
Santiagolemoine <slemoine@udesa.edu.ar>
2020-06-16 22:25:18 -0400

    parte del js del album

--- albums/albums.js
+++ albums/albums.js
@@ -5,7 +5,7 @@ window.addEventListener("load", function() {
     let codigoDelAlbum = queryString.get("albumID");
  
 
-    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/" + codigoDelAlbum)
+    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/302127" + codigoDelAlbum)
     .then(
         function(respuesta) {
             return respuesta.json();            
@@ -13,11 +13,52 @@ window.addEventListener("load", function() {
     )
     .then(
         function(informacion) {
-            let resultadosAlbum = informacion.data
-            let album = resultadosAlbum.title
-            let img =  resultadosAlbum.cover
-            let fans = resultadosAlbum.fans
+            console.log(informacion)
             
-        }
-    )
-})
+            let album = informacion.title
+            let img =  informacion.cover
+            let fans = informacion.fans
+            let tiempo = informacion.duration 
+
+            let htmlimg = `
+            <figure>
+            <div><img class="portada" src="`+ img +`" alt="Álbum Anti"></div>
+            <div class="i"><i class="far fa-play-circle"></i></div>
+            </figure>
+            <div class="info-container">
+            <h1>`+ album +`</h1>
+            <ul class="datos-album">
+            <li> `+ tiempo +`  segundos</li>
+            <li>`+ fans +` fans</li>
+            </ul>
+           </div>
+       
+            `
+             document.querySelector("section").innerHTML += htmlimg  
+
+             let albunes = informacion.tracks.data;
+                
+                for (let index = 0; index < 9; index++) {
+    
+                const cadacancion = albunes[index];
+                
+                
+                let song = cadacancion.title;;
+                
+    
+                    let htmlNuevo = `
+                    <article class="canciones">
+                    <div class="numero">1</div>
+                    <div><i class="fas fa-play"></i></div>
+                    <div  class="play">`+ song +`</div>
+                    <div> 03:39 </div>
+                    <div><i class="fas fa-heart"></i></div>
+                    </article> 
+                    `
+                    
+                    document.querySelector("main").innerHTML += htmlNuevo
+                }
+            }
+       )
+       
+   })
